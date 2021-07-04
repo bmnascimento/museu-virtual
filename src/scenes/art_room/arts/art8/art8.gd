10f5355e4261aa41928b44ba4e1a64fbee02c287
Felipe Sibuya <felipe.sibuya@exame.com>
2021-07-04 19:35:27 -0300

    Structure 1
    Todos os quadros organizados usando o modelo default

---
+++ src/scenes/art_room/arts/art8/art8.gd
@@ -0,0 +1,24 @@
+extends Area
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass # rotate_y(deg2rad(180 * delta))
+
+
+func _on_Area2D_body_entered(body):
+	queue_free()
+
+
+func _on_art1_body_entered(body):
+	if body is KinematicBody:
+		pass
